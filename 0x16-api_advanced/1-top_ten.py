05f47c99d9d38855e07bb826cd13b880a0ad71b2
kimani kamau <kimanikamau971@gmail.com>
2025-03-11 09:40:56 +0300

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,9 @@ def top_ten(subreddit):
     headers = {"User-Agent": "MyRedditBot/1.0 (by u/example_user)"}
     params = {"limit": 10}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False
+            )
 
     if response.status_code != 200:
         print("None")
