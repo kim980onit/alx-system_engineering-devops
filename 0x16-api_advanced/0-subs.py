9705508e80ad50943bd2b3d330c6de69c316e659
kimani kamau <kimanikamau971@gmail.com>
2025-03-11 08:55:05 +0300

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,12 @@ from requests import get
 
 def number_of_subscribers(subreddit):
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; RedditAPI/1.0)"}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        return response.json().get("data", {}).get("subscribers", 0)
+
         return 0
